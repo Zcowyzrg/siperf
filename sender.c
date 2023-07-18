4109d4bfe0c6f7bc12c1b9678da3f7e876327e49
Maciej Grzywocz <maciej.grzywocz@yahoo.com>
2023-07-18 23:06:47 +0100

    Send bulk data

--- sender.c
+++ sender.c
@@ -12,9 +12,11 @@
 #include <netinet/in.h>
 #include <netdb.h> 
 
-#define BUFSIZE 1024
+#define BUFSIZE 3 * 512
 
-/* 
+unsigned char static_buffer[3*512];
+
+/**
  * die - wrapper for perror
  */
 void die(const char *msg) {
@@ -23,15 +25,15 @@ void die(const char *msg) {
 }
 
 int main(int argc, char **argv) {
-  char buf[BUFSIZE];
 
   /* check command line arguments */
-  if (argc != 3) {
-    fprintf(stderr, "Usage: %s <hostname> <port>\n", argv[0]);
+  if (argc != 4) {
+    fprintf(stderr, "Usage: %s <hostname> <port> <count>\n", argv[0]);
     exit(0);
   }
   char* hostname = argv[1];
   int portno = atoi(argv[2]);
+  int packet_count = atoi(argv[3]);
 
   /* socket: create the socket */
   int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
@@ -54,16 +56,13 @@ int main(int argc, char **argv) {
   memcpy(&serveraddr.sin_addr.s_addr, (char *)server->h_addr, server->h_length);
   serveraddr.sin_port = htons(portno);
 
-  /* get a message from the user */
-  memset(buf, 0, BUFSIZE);
-  printf("Please enter msg: ");
-  fgets(buf, BUFSIZE, stdin);
-
   /* send the message to the server */
-  int n = sendto(sockfd, buf, strlen(buf), 0,
-                 (const struct sockaddr*)&serveraddr, sizeof(serveraddr));
-  if (n < 0) {
-    die("ERROR in sendto");
+  while (packet_count--) {
+    int n = sendto(sockfd, static_buffer, BUFSIZE, 0,
+                   (const struct sockaddr*)&serveraddr, sizeof(serveraddr));
+    if (n < 0) {
+      die("ERROR in sendto");
+    }
   }
   
   return 0;
